dbd695f8e6b629ef40ab951be5b92b7d8995f108
MoshiLi <mo-shi.li@ubisoft.com>
2020-01-03 17:51:13 +0800

    initial

--- test/index.js
+++ test/index.js
@@ -4,27 +4,55 @@ import TigrexDOM from "../src/TigrexDOM";
 
 const App1 = () => {
   return (
-    <div id="3">
-      <p>hello</p>
-      <p>world</p>
-      inline_as_const
+    <div>
+      <p>App1</p>
     </div>
   );
 };
-const Test1 = () => {
-  return <p>!</p>;
-};
-const Test2 = <div>312</div>;
-const App2 = (
-  <div id="213">
-    <p>hello</p>
-    <p>world</p>
-    <Test1 id="1" />
-    inline_as_const
-    <Test2 id="2" />
+
+class App2 extends Tigrex.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 1
+    };
+  }
+  add() {
+    this.setState({ number: this.state.number + 1 });
+  }
+  minus() {
+    this.setState({ number: this.state.number - 1 });
+  }
+  render() {
+    return (
+      <div className="container">
+        <p style={"padding: '100px'"}>{this.state.number}</p>
+        <Display number={this.state.number} />
+        <button onClick={this.add.bind(this)}>+</button>
+        <button onClick={this.minus.bind(this)}>-</button>
+      </div>
+    );
+  }
+}
+
+class Display extends Tigrex.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <p>+{this.props.number}+</p>;
+  }
+}
+
+const App3 = (
+  <div className="container">
+    <p>APP3</p>
+    <p>Hi, there</p>
   </div>
 );
 
-TigrexDOM.render(<App1 id="yahah" />, document.getElementById("root"));
 console.log("=========");
-//ReactDOM.render(<App2 />, document.getElementById("root"));
+//TigrexDOM.render(<App1 id="app1" />, document.getElementById("root"));
+TigrexDOM.render(<App2 id="app2" />, document.getElementById("root"));
+//TigrexDOM.render(<App3 id="app3" />, document.getElementById("root"));
+console.log("=========");
